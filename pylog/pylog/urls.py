74b21d011802bc08c8e9daff9efd1be33c9bebe8
이상용 <lsy3709@naver.com>
2025-03-11 14:34:02 +0900

    5교시, pylog 화면 연결 2, urls,py , html 화면 추가

--- pylog/pylog/urls.py
+++ pylog/pylog/urls.py
@@ -17,9 +17,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from blog.views import post_list
 from pylog.views import index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index),
+    path('posts/', post_list),
+
 ]
