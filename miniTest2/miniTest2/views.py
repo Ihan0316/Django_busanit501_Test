84701b39df1f71f451aca073b60d802b01ddbf5e
이상용 <lsy3709@naver.com>
2025-03-12 09:46:49 +0900

    1교시, 미니실습 풀이겸, 전체 복습

---
+++ miniTest2/miniTest2/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+
+def main(request):
+    # return HttpResponse("안녕하세요. 장고 웹 프레임워크~~ Hello World")
+    return render(request, "main.html")
+
+def lunch_list(request):
+    # return HttpResponse("lunch_list : 점심 메뉴입니다.")
+    return render(request, "lunch_list.html")
