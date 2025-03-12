29c495ee271c0f84574b5e7fcc73db3a291bdd7c
이상용 <lsy3709@naver.com>
2025-03-12 14:21:26 +0900

    5교시, 글_댓글 작성 , 화면, 로직처리 2, 서버에서 넘어온 데이터 1차 확인

--- pylog/blog/views.py
+++ pylog/blog/views.py
@@ -19,6 +19,15 @@ def post_detail(request,post_id):
     }
     return render(request, 'post_detail.html',context)
 
+def post_add(request):
+    if request.method == 'POST':
+        title = request.POST['title']
+        content = request.POST['content']
+        print(title)
+        print(content)
+    else:
+        print("method GET")
+    return render(request, 'post_add.html')
 
 
 
--- pylog/pylog/urls.py
+++ pylog/pylog/urls.py
@@ -17,7 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from blog.views import post_list, post_detail
+from blog.views import post_list, post_detail, post_add
 from pylog.views import index
 
 from django.conf import settings
@@ -27,7 +27,9 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index),
     path('posts/', post_list),
-    path('posts/<int:post_id>/', post_detail)
+    path('posts/<int:post_id>/', post_detail),
+    path('posts/add/',post_add),
+
 
 ]
 
