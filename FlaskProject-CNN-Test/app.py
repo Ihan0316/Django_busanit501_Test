1862087744febe785a852d20873b48c7268dbfb7
이상용 <lsy3709@naver.com>
2025-03-25 11:07:12 +0900

    3교시, YOLOv8, 이미지 , 영상 객체 탐지 플라스크 서버 작업4, 리팩토링1, 프로그레스 바 추가, 소켓 경고 제거

--- FlaskProject-CNN-Test/app.py
+++ FlaskProject-CNN-Test/app.py
@@ -31,7 +31,8 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 
 # ✅ YOLO 모델 로드
-yolo_model = YOLO("model/best-busanit501-aqua.pt")
+# yolo_model = YOLO("model/best-busanit501-aqua.pt")
+yolo_model = YOLO("model/best-apple-test.pt")
 # app.config['SERVER_NAME'] = '10.100.201.87:5000'  # Flask 서버 주소와 포트 설정
 
 # ✅ 결과 저장 폴더 설정
